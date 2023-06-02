ccd5fd28f78e2b3e6f361bc48136daeda3a35664
Nmargolis00 <119348315+Nmargolis00@users.noreply.github.com>
2023-06-01 19:36:56 -0600

    fixed bug

--- client/src/routes/AddressAutocomplete.jsx
+++ client/src/routes/AddressAutocomplete.jsx
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import React, { useState } from 'react'
 import { GeoapifyGeocoderAutocomplete, GeoapifyContext } from '@geoapify/react-geocoder-autocomplete'
 import '@geoapify/geocoder-autocomplete/styles/minimal.css'
-
+dotenv.config();
 const { APIKEY } = process.env;
 
  
