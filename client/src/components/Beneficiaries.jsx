654bf61c96386b25d98bf2057f7618a84fcaf544
Jeff <118331364+Jerbert16@users.noreply.github.com>
2023-06-07 21:04:16 -0600

    button color change

--- client/src/components/Beneficiaries.jsx
+++ client/src/components/Beneficiaries.jsx
@@ -275,7 +275,7 @@ export default function Beneficiaries() {
                         value={newBene.percentage}
                         onChange={handleInputChange}
                       />
-                      <Button type="submit" variant="contained">
+                      <Button style={{backgroundColor: "#0B746C", color: "white"}} type="submit" variant="contained">
                         Save
                       </Button>
                     </Box>
